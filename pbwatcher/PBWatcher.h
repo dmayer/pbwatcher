9fd551b817da693404641421e3dbdbf6cf65668a
Daniel A. Mayer <daniel@matasano.com>
2014-01-06 08:19:44 -0600

    initial commit. Works fine

---
+++ pbwatcher/PBWatcher.h
@@ -0,0 +1,27 @@
+//
+//  PBWatcher.h
+//  pbwatcher
+//
+//  Created by Daniel Mayer on 12/3/13.
+//
+//
+#ifndef pbwatcher_PBWatcher_h
+#define pbwatcher_PBWatcher_h
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+#import <CoreFoundation/CoreFoundation.h>
+
+
+@interface PBWatcher : UIApplication {
+
+}
+
+- (void) applicationDidFinishLaunching:
+(NSNotification *) aNotification;
+
+@end
+
+
+#endif
---
+++ pbwatcher/PBWatcher.m
@@ -0,0 +1,44 @@
+//
+//  main.m
+//  pbwatcher
+//
+//  Created by Daniel Mayer on 12/3/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+int main (int argc, const char * argv[])
+{
+    // first command line argument is the sleep time
+    int sleep_time =  [[NSString stringWithUTF8String:argv[1]] intValue];
+    
+    //TODO: subscribe to changes instead
+    // pasteboard names are passed as command line arguments starting at 2.
+    // store them in array
+    NSMutableArray *pb_names = [[NSMutableArray alloc] init];
+
+    for (int i=2; i<argc; i++) {
+        NSString *str = [NSString stringWithUTF8String:argv[i]];
+        [pb_names addObject:str];
+    }
+
+    UIApplicationInitialize();
+    
+    while(true) {
+        // general pasteboard
+        UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+        NSLog(@"general:%@",pasteboard.string);
+        
+        // retrieve data for all other configured pasteboards
+        for (int i=0; i<[pb_names count]; i++) {
+            UIPasteboard *pasteboard = [UIPasteboard pasteboardWithName:pb_names[i] create:NO ];
+            NSLog(@"%@:%@", pb_names[i], pasteboard.string);
+        }
+        
+        sleep(sleep_time);
+    }
+    exit(0);
+    
+}
